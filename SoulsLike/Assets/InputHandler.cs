c10065f58942e8e48f83c5d7ab9520a103b6b937
JeffJi <jjc1114@sina.com>
2021-10-14 18:06:48 +0800

    Player can Run

--- SoulsLike/Assets/InputHandler.cs
+++ SoulsLike/Assets/InputHandler.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace JeffJi
+{
+    public class InputHandler : MonoBehaviour
     {
-        
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        PlayerControls inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        public void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovement.Movement.performed += j => movementInput = j.ReadValue<Vector2>();
+                //只要movement执行了，就把movement里获得到的Vector2赋值给movementInput，这样就知道WASD动了多少了。
+                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
     }
 }
+
+
